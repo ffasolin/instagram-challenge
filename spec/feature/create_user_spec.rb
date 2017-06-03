3a48338c37277e794ba38be0f1ada3bff7c03e10
Filipe Fasolin <ffasolin@gmail.com>
2017-06-03 20:34:29 +0100

    Fix test for signup form

--- spec/feature/create_user_spec.rb
+++ spec/feature/create_user_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.feature "Create user", type: :feature do
   scenario 'user can sign up' do
     visit '/'
-    fill_in 'users[name]', with: 'User'
-    fill_in 'users[username]', with: 'username'
-    fill_in 'users[email]', with: 'user@user.com'
-    fill_in 'users[password]', with: '123'
+    fill_in 'user[name]', with: 'User'
+    fill_in 'user[username]', with: 'username'
+    fill_in 'user[email]', with: 'user@user.com'
+    fill_in 'user[password]', with: '123'
     click_button 'Sign up'
-    expect(page).to have_content 'Welcome, User!'
+    expect(page).to have_content 'User, user@user.com'
   end
 end
